9e678afc4c5d9da7eefd0eb58a15cfc60b9ed713
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-12-02 11:54:35 -0700

    Add react css transition group to allow transitions between pages.

--- app/templates/main.js
+++ app/templates/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ReactCssTransitionGroup from 'react-addons-css-transition-group';
 import HeaderComponent from '../components/header.js';
 
 class MainTemplate extends React.Component {
@@ -8,7 +9,12 @@ class MainTemplate extends React.Component {
       <div>
         <HeaderComponent />
         <div className="app-wrapper">
-            {this.props.children}
+          <ReactCssTransitionGroup
+            transitionName="appear"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+              {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+            </ReactCssTransitionGroup>
         </div>
       </div>
     )

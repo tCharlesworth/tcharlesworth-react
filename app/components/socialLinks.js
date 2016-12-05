4f949d07205a0e9df5be2e26217dd47a011554ad
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-12-04 19:32:03 -0700

    Increase social links size

--- app/components/socialLinks.js
+++ app/components/socialLinks.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SocialLinksComponent extends React.Component {
     render() {
         return (
-            <ul className="social-box bottom-left">
+            <ul className="social-box bottom-left fa-2x">
                 <li><a href="http://www.twitter.com"><i className="fa fa-twitter"></i></a></li>
                 <li><a href="http://www.github.com"><i className="fa fa-github"></i></a></li>
             </ul>

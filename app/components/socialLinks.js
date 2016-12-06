b652f35193832a012be56fe388eec2aeccdb4839
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-12-06 09:26:44 -0700

    Added a social link for linked in and added the proper urls to each of the social links.

--- app/components/socialLinks.js
+++ app/components/socialLinks.js
@@ -4,8 +4,9 @@ class SocialLinksComponent extends React.Component {
     render() {
         return (
             <ul className="social-box bottom-left fa-2x">
-                <li><a href="http://www.twitter.com"><i className="fa fa-twitter"></i></a></li>
-                <li><a href="http://www.github.com"><i className="fa fa-github"></i></a></li>
+                <li><a href="https://twitter.com/t1m0thyC" target="_blank"><i className="fa fa-twitter"></i></a></li>
+                <li><a href="https://github.com/tCharlesworth" target="_blank"><i className="fa fa-github"></i></a></li>
+                <li><a href="https://www.linkedin.com/pub/tim-charlesworth/104/139/6b8" target="_blank"><i className="fa fa-linkedin"></i></a></li>
             </ul>
         )
     }

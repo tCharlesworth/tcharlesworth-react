2af9241ada7e44b80c822ce738247229b0b2e0af
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-01-05 11:20:08 -0700

    Add picture to about page.

--- app/views/about.js
+++ app/views/about.js
@@ -6,7 +6,10 @@ class AboutView extends React.Component {
             <div className="wrapper">
                 <p className="big-text page-padding-half text-blue">About</p>
                 <div className="container">
-                    <p>
+                    <div className="right half">
+                        <img className="right img-responsive img-max-height" src={ "./assets/timface.jpg" } />
+                    </div>
+                    <p className="left half paragraph">
                         Hi i'm Tim Charlesworth I was born and raised in Utah and I love programming. After writing 
                         simple code on my TI-84 calculator in high school I pursued an interest in Computer Science learning C# and 
                         C++. I was drawn to web development because it is an industry constantly striving to find the best programming

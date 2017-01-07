83834035a1c25c8843471a88b631886ef333295e
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-01-07 16:13:59 -0700

    Fix server to server files from current directory, not to look for another build folder.

--- app/server.js
+++ app/server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 
-app.use('/', express.static( __dirname+"/build" ));
+app.use('/', express.static( __dirname ));
 app.listen( 3000, function() { console.log("Running") } );

3d8685a4fd298f8d93e09802adcb4ec69f3902bf
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-01-30 13:54:03 -0700

    remove resume file require

--- buildEntry.js
+++ buildEntry.js
@@ -1,4 +1,3 @@
 require('./app/index.js');
 require('file?name=index.[ext]!./app/index.html');
 require('file?name=server.[ext]!./app/server.js')
-require('file?name=TimCharlesworthResume.[ext]!./resources/TimCharlesworthResume.pdf');
